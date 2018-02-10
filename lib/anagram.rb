49975731fa6c488b89183793625f32f36652f557
Nicko Dela Cruz <nickodc96@gamil.com>
2018-02-09 17:19:23 -0800

    add sorter method for input

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,9 @@
 class Anagram
+  def initialize(word_one, word_two)
+    @word_one = word_one
+    @word_two = word_two
+  end
+  def sorter(input)
+    characters=input.split("")
+    characters_sort=characters.sort
+  end
