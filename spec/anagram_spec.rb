65d2a1a5792d3cab230e673b26c6077a15f88b63
Nicko Dela Cruz <nickodc96@gamil.com>
2018-02-09 17:17:45 -0800

    add first rule to check for english words

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,2 +1,8 @@
 require("rspec")
 require("anagram")
+
+describe('#anagram') do
+  it("Checks string for english words") do
+    expect(Anagram.new("english","word").english_check("english","word")).to(eq(true))
+  end
+end
