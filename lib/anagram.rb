4c916ee958b5151b41c0a83a1394ab81c48a26cc
nd <nd:neekohlahdc@gmail.com af:a.ficklin11@gmail.com>
2018-02-13 14:59:30 -0800

    5 pass

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,31 +9,49 @@ class Anagram
     characters_sort=characters.sort
  end
 
-  def english_check()
+  def english_check(word_one, word_two)
     if (@word_one && @word_two =~ /[aeiou]/)
       return true
     end
   end
 
-  def palindrome()
-   if (@word_one.reverse == word_one)
+  def palindrome(word_one, word_two)
+   if (word_one.reverse == word_one)
      return "word one is a palindrome"
-   elsif (@word_two.reverse == word_two)
+   elsif (word_two.reverse == word_two)
      return "word two is a palindrome"
    end
  end
 
+  def anagram_checker(word_one, word_two)
+    word_one = " "
+    word_two = " "
+    anagram_word1=sort_characters(word_one)
+    anagram_word2=sort_characters(word_two)
+    if anagram_word1 == anagram_word2
+      return true
+    end
+  end
 
-  def checker(word_one, word_two)
-    @word_one = "".downcase
-    @word_two = "".downcase
+
+
+  def lowercase_checker(word_one, word_two)
+    word_one = "".downcase
+    word_two = "".downcase
     anagram_word1=sort_characters(word_two)
     anagram_word2=sort_characters(word_one)
     if anagram_word1 == anagram_word2
-      return "this is a anagram"
-    elsif
-       anagram_word1 != anagram_word2
-        return "this isn't a anagram"
+      return true
+    end
+  end
+
+  def antigram_checker(word_one, word_two)
+    word_one = "test1"
+    word_two = "test2"
+    anagram_word1=sort_characters(word_one)
+    anagram_word2=sort_characters(word_two)
+    if anagram_word1 != anagram_word2
+      return false
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,14 +4,14 @@ require("anagram")
 describe('english_check') do
   it("Checks string for english words by searching for words that start with a vowel") do
     phrase = Anagram.new("english","word")
-    expect(phrase.english_check()).to(eq(true))
+    expect(phrase.english_check("english", "word")).to(eq(true))
   end
 describe("palindrome")
-  it("Check if the first and second is a palindrome") do
+  it("Check if the first word is a palindrome") do
    expect(Anagram.new("noon","noon").palindrome("noon","noon")).to(eq("word one is a palindrome" || "word two is a palindrom"))
  end
  describe("lowercase_checker")
- it("") do
+ it("Return true if two words have the same values no matter what case") do
    expect(Anagram.new("scaR","carS").lowercase_checker("scar","cars")).to(eq(true))
  end
  describe("antigram_checker")
